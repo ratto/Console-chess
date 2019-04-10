b5f8b1e23a4edc4cfa55ae1e9afa3a1e99166790
ratto <rattopedro@gmail.com>
2019-04-10 12:00:47 -0300

    Position class created

---
+++ Console-chess/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Board;
+
+namespace Console_chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p = new Position(3, 4);
+
+            Console.WriteLine("Position: " + p);
+        }
+    }
+}
